74e653a96ff41b63cc4ecfb1d8cd712f9c25d73e
Adam <adam.ranieri@revature.com>
2023-02-07 16:28:26 -0500

    added potlukk by id

--- resolvers/queries.py
+++ resolvers/queries.py
@@ -4,7 +4,14 @@ from models.enums import *
 from app_data import lukkers, potlukks, notification_queue
 
 
-def potlukks_resolver() -> list[Potlukk]:
+def potlukks_resolver(potlukkId: int | None = None) -> list[Potlukk]:
+
+    if potlukkId:
+        if potlukk := potlukks.get(potlukkId):
+            return [potlukk]
+        else:
+            return []
+
     return list(potlukks.values())
 
 def notification_resolver() -> list[PotlukkNotification]:
